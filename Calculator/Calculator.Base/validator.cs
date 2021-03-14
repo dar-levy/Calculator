cd91dd47a03aed8a8b3ca344a7626b79d75b8610
Dar <Levy>
2021-03-14 16:36:49 +0200

    Create new class and set validator fields

---
+++ Calculator/Calculator.Base/validator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Calculator.Base
+{
+    public class Validator
+    {
+        private int _openParensCounter;
+        private int _closeParensCounter;
+        private char _previous = '0';
+    }
+}
