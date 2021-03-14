cfed33120f4866f85439646aa93ce8f799231cbe
Dar <Levy>
2021-03-14 18:42:36 +0200

    Create SplitToTokens method

--- Calculator/Calculator.Base/Parser.cs
+++ Calculator/Calculator.Base/Parser.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Calculator.Base.config;
 using Calculator.Core.Entities;
 
 namespace Calculator.Base
@@ -19,6 +20,47 @@ namespace Calculator.Base
         private string Trim(string stringWithSpaces)
         {
             return stringWithSpaces.Replace(" ","");
+        }
+        
+        private void SplitToTokens(string str)
+        {
+            for (var index = 0; index < str.Length; index++)
+            {
+                var item = str[index];
+                var priority = EvalPriority();
+                
+                if (item == '(')
+                {
+                    _countOpenParenthesis++;
+                }
+                else if (item == ')')
+                {
+                    _countCloseParenthesis++;
+                }
+                else if (char.IsDigit(item))
+                {
+                    var completeNumber = string.Empty;
+                    var counter = 0;
+                    while (index+counter < str.Length && char.IsDigit(str[index+counter]))
+                    {
+                        completeNumber += str[index + counter];
+                        counter++;
+                    }
+                    var token = new Token(completeNumber, priority);
+                    _tokens.Add(token);
+                    index += --counter;
+                }
+                else if (Config.Operators.Contains(item))
+                {
+                    var token = new Token(item.ToString(), priority);
+                    _tokens.Add(token);
+                }
+            }
+        }
+        
+        private int EvalPriority()
+        {
+            return _countOpenParenthesis - _countCloseParenthesis;
         }
     }
 }
