52167f116e71a29f993785a51951a21b892fcd53
Dar <Levy>
2021-03-14 18:38:23 +0200

    Create fields

--- Calculator/Calculator.Base/Parser.cs
+++ Calculator/Calculator.Base/Parser.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+using Calculator.Core.Entities;
+
 namespace Calculator.Base
 {
     public class Parser
     {
-        
+        private int _countOpenParenthesis;
+        private int _countCloseParenthesis;
+        private List<Token> _tokens;
+        private Stack<Token> _tokenStack;
     }
 }
