691653829fbcfe865c1cfa751551a686abf78adf
Dar <Levy>
2021-03-14 16:37:43 +0200

    Create CheckUnknownToken method

--- Calculator/Calculator.Base/validator.cs
+++ Calculator/Calculator.Base/validator.cs
@@ -7,5 +7,14 @@ namespace Calculator.Base
         private int _openParensCounter;
         private int _closeParensCounter;
         private char _previous = '0';
+        
+        private void CheckUnknownToken(char item)
+        {
+            if (!char.IsDigit(item) && !Config.Operators.Contains(item) && item !='(' && item != ')')
+            {
+                throw new Exception(
+                    $"An unknown character '{item}' in your equation, Please delete it and try again");
+            }
+        }
     }
 }
